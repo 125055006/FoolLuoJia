d142a754874223abc29ac7bb3d01c15736d96baa
XXX-huhu <2042236941@qq.com>
2025-12-02 14:45:23 +0800

    0

--- MyClient.h
+++ MyClient.h
@@ -9,10 +9,8 @@ class MyClient : public QObject
     Q_OBJECT
 public:
     explicit MyClient(QObject *parent=nullptr);
-    QTcpSocket* getSocket() const {return socket;} //暴露socket供外部连接
     void connectToServer(const QString &host,quint16 port);
     void sendMessage(const QString &message);
-    void sendData(const QByteArray& data);
 private slots:
     void onConnected();
     void onReadyRead();
--- MyServer.h
+++ MyServer.h
@@ -13,7 +13,6 @@ public:
     explicit MyServer(QObject *parent =nullptr);
     bool startServer(quint16 port);      //启动服务器
     void sendToAllClients(const QString &message);    //向所有客户端发信息
-    void sendToAllClients(const QByteArray &data);
 private slots:
     void onNewConnection();    //处理新客户端的连接
     void onClientReadyRead();    //读取客户端的数据
